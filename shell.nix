bd99d676f5dfea18b15c7d0f6bc406a8abc9ea48
Vincent <j.vinc-sites@yahoo.com>
2026-04-13 19:08:07 +0200

    Update shell.nix and gitignore

--- shell.nix
+++ shell.nix
@@ -3,6 +3,18 @@
 pkgs.mkShell {
   buildInputs = [
     pkgs.qemu
-    pkgs.rustc
+    pkgs.jq
+    pkgs.pkgsCross.aarch64-multiplatform.buildPackages.gcc
+    pkgs.pkgsCross.aarch64-multiplatform.buildPackages.binutils
+    pkgs.lld
+    pkgs.rustup
   ];
+
+  CC = "aarch64-unknown-linux-gnu-gcc-14";
+  AS = "aarch64-unknown-linux-gnu-as";
+
+  shellHook = ''
+    rustup target add aarch64-unknown-none
+    alias make="make CC=aarch64-unknown-linux-gnu-gcc-14 AS=aarch64-unknown-linux-gnu-as"
+  '';
 }
