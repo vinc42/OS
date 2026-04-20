cb105912d07426dced79a6f365998745bd3e063d
Vincent <j.vinc-sites@yahoo.com>
2026-04-20 13:21:11 +0200

    getchar waits for a new char

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,17 @@
 use core::panic::PanicInfo;
 
 const UART: *mut u8 = 0x0900_0000 as *mut u8;
+const UART_FR: *mut u32 = (0x0900_0000 + 0x18) as *mut u32;
+
 
 #[unsafe(no_mangle)]
 pub extern "C" fn kmain() {
     print(b"Hello, from Rust!\n");
     loop {
-        putchar(getchar());
+        let c = getchar();
+        print(b"my char : ");
+        putchar(c);
+        print(b"\n");
     }
 }
 
@@ -27,8 +32,10 @@ fn print(s: &[u8]) {
 
 fn getchar() -> u8   {
     unsafe {
-        let value: u8 = *UART;
-        return value;
+        while (*UART_FR & 0x10) != 0 {
+            core::arch::asm!("nop");
+        }
+        *UART
     }
 }
 
